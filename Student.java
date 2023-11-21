c00bcf481609521c03d7f73d698487ace0c771e9
ryanchiu727 <151646504+ryanchiu727@users.noreply.github.com>
2023-11-21 08:54:01 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,3 +1,20 @@
 public class Student {
     public String firstName;
+    public String animal;
+    public Student(String name, String favFood)
+    {
+      firstName = name;
+      food = favFood;
+    }
+    public String getName()
+    {
+      return firstName;
+    }
+    public String favFood()
+    {
+      return animal;
+    }
+    public String toString()
+    {
+      return firstName + "'s favourite food is " + favFood;
 }
